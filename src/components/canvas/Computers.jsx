ea51750d383d63ac8e47eaf5a2f0723d6c0ec60d
Yashwanth Kumar <yash.madenenni@outlook.com>
2023-12-05 09:53:13 +0000

    Update Navigation bar and fix tailwindcss

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import {React, Suspense, useEffect, useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    
   )
 }
 
